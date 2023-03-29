fdf005f8a7e819feb06a0e3df4bdf5a9dabdf9f1
Marco Cruz <marcoxcruz2002@gmail.com>
2023-03-29 08:27:58 +0100

    first commit

---
+++ Main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('C:\\Users\\marco\\Documents\\VsCode\\python\\Trabalho\\US_Accidents_Dec21_updated.csv')
+
+print (df)
