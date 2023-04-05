a8eb3df137d1046cc8c3eb5fa2778711cb5a2ac4
Sousix7 <diogorpsousa10@gmail.com>
2023-04-05 20:47:17 +0100

    Acidentes Por estado E Acidentes nas diferentes timezones

--- Main.py
+++ Main.py
@@ -5,17 +5,10 @@ import seaborn as sns
 df = pd.read_csv('US_Accidents_Dec21_updated.csv')
 new_df = df.fillna(0)
 
-df1 = df.DataFrame(df) 
-print(df1)
+#Acidentes Por estado
+countsAciSta = new_df["State"].value_counts()
+print(countsAciSta)
 
-df2 = df.describe()# Ordena por quartis (25%, 50%(mediana), 75%)
-print(df2)
-
-df3 = df.T #Transpoem a tabela
-print(df3)
-
-df4 =  df.size #diz o tamanho
-print(df4)
-
-df5 = df.mean() #diz a media de acidentes por ano falta fazer o filtro
-print("Media de acidentes por ano",df5)
+#Acidentes nas diferentes timezones
+timezone_counts = new_df['Timezone'].value_counts()
+print(timezone_counts)
