4e6a77ad6aabd4465d055f8d634e17a211faa480
DiogoCanito <canitodiogo@gmail.com>
2023-04-08 16:33:22 +0100

    Média de Humidade por nível de Severidade

--- Main.py
+++ Main.py
@@ -1,12 +1,18 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('C:\\Users\\marco\\Documents\\VsCode\\python\\Trabalho\\US_Accidents_Dec21_updated.csv')
+df = pd.read_csv('US_Accidents_Dec21_updated.csv')
+new_df = df.fillna(0)
 
-print (df)
+severity_humidity = new_df.groupby('Severity')['Humidity(%)'].mean()
+print(severity_humidity)
 
-
+#Se quiseremos fazer correação
+#humidity_severity = new_df[['Humidity(%)', 'Severity']]
+#corr_coef = np.corrcoef(humidity_severity['Humidity(%)'], humidity_severity['Severity'])[0][1]
+#print(corr_coef)
 
 
 
