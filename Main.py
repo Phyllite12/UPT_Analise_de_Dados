ba70f8c0db779b5e727d9a977e5ef48e5483caad
Sousix7 <diogorpsousa10@gmail.com>
2023-04-12 10:39:09 +0100

    #HISTOGRAMA E CONTAGEM DO NºACIDENTES POR MÊS

--- Main.py
+++ Main.py
@@ -7,8 +7,22 @@ new_df = df.fillna(0)
 
 #Acidentes Por estado
 countsAciSta = new_df["State"].value_counts()
-print(countsAciSta)
+#print(countsAciSta)
 
 #Acidentes nas diferentes timezones
 timezone_counts = new_df['Timezone'].value_counts()
-print(timezone_counts)
+#print(timezone_counts)
+
+#HISTOGRAMA E CONTAGEM DO NºACIDENTES POR MÊS
+new_df['Start_Time'] = pd.to_datetime(df['Start_Time']) # converte a coluna de data em um objeto de série de data
+new_df['Month'] = new_df['Start_Time'].dt.month # extrai o mês de cada data na coluna "Start_Time"
+print(new_df['Month'].value_counts()) #Contagem do nºde acidentes por mês
+plt.hist(new_df['Month'], bins=12)
+
+# adiciona títulos e rótulos aos eixos
+plt.title('Distribuição dos Acidentes de Carro ao Longo dos Meses')
+plt.xlabel('Mês')
+plt.ylabel('Número de Acidentes')
+
+# exibe o histograma
+plt.show()
