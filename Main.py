890a305ee7affef7b52aabd2d0f154240499de47
Marco Cruz <marcoxcruz2002@gmail.com>
2023-04-14 15:43:16 +0100

    correções e mais tabelas feitas

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from datetime import datetime as dt
+from sklearn.preprocessing import MinMaxScaler
+
 
 df = pd.read_csv('US_Accidents_Dec21_updated.csv')
 new_df = df.fillna(0)
@@ -17,97 +19,229 @@ df1['Temperature(C)'] = (df['Temperature(F)'] - 32) * 5/9
 #converter a distancia de milhas para metros
 df1['Distance(M)'] = (df['Distance(mi)'] * 1609.34)
 
-# Ordena a severidade por quartis (25%, 50%(mediana), 75%)
-describSever = df.describe()
-print(describSever)
-
-#Acidentes Por estado
-countsAciSta = df1["State"].value_counts()
-#print(countsAciSta)
-
-#Acidentes nas diferentes timezones
-timezone_counts = df1['Timezone'].value_counts()
-#print(timezone_counts)
-
-severity_humidity = df1.groupby('Severity')['Humidity(%)'].mean()
-#print(severity_humidity)
-
-#Se quiseremos fazer correação
-humidity_severity = df1[['Humidity(%)', 'Severity']]
-corr_coef = np.corrcoef(humidity_severity['Humidity(%)'], humidity_severity['Severity'])[0][1]
-#print(corr_coef)
-
-
-sns.distplot(df1['Severity'], kde=False, bins=10)
-plt.show()
-
-#cria grafico de dispersão entre a severidade e a temperatura em fahrenheit
-#sns.pairplot(df1[['Severity', 'Humidity(%)', 'Temperature(F)', 'Visibility(mi)']])
-#plt.show()
-
-
-
-#HISTOGRAMA E CONTAGEM DO NºACIDENTES POR MÊS
-df1['Start_Time'] = pd.to_datetime(df['Start_Time']) # converte a coluna de data em um objeto de série de data
-df1['Month'] = df1['Start_Time'].dt.month # extrai o mês de cada data na coluna "Start_Time"
-print(df1['Month'].value_counts()) #Contagem do nºde acidentes por mês
-#plt.hist(new_df['Month'], bins=12)
-
-#Adiciona títulos e rótulos aos eixos
-plt.title('Distribuição dos Acidentes de Carro ao Longo dos Meses')
-plt.xlabel('Mês')
-plt.ylabel('Número de Acidentes')
-
-# exibe o histograma
-#plt.show()
-
-
-#BOXPLOTS------------------------------
-#SEVERIDADE
-# cria um boxplot da coluna "Severity"
-plt.boxplot(df1['Severity'])
-
-# adiciona títulos e rótulos aos eixos
-plt.title('Distribuição da Severidade dos Acidentes')
-plt.ylabel('Severidade')
-
-# exibe o boxplot
-plt.show()
-
-#HUMIDADE-------------------------------------------------
-# cria um boxplot da coluna "Humidity(%)"
-plt.boxplot(df1['Humidity(%)'])
-
-# adiciona títulos e rótulos aos eixos
-plt.title('Distribuição da Severidade dos Acidentes')
-plt.ylabel('Humidade(%)')
-
-# exibe o boxplot
-plt.show()
-
-#TEMPERATURA-------------------------------------------
-#cria um boxplot da coluna "Temperature(C)"
-plt.boxplot(df1['Temperature(C)'])
-
-# adiciona títulos e rótulos aos eixos
-plt.title('Distribuição da Temperatura no Momento dos Acidentes')
-plt.ylabel('Temperatura (C)')
-
-# exibe o boxplot
-plt.show()
-
 #transformar os dados categóricos em numéricos
 df1['Sunrise_Sunset'] = df['Sunrise_Sunset'].map({'Day': 1, 'Night': 0})
-df1['Crossing'] = df['Crossing'].map({'True': 1, 'False': 0})
-df1['Traffic_Signal'] = df['Traffic_Signal'].map({'True': 1, 'False': 0})
+df1['Crossing'] = df['Crossing'].map({True: 1, False: 0})
+df1['Traffic_Signal'] = df['Traffic_Signal'].map({True: 1, False: 0})
+df1['Amenity'] = df['Amenity'].map({True: 1, False: 0})
+df1['Bump'] = df['Bump'].map({True: 1, False: 0})
+df1['Give_Way'] = df['Give_Way'].map({True: 1, False: 0})
+df1['Junction'] = df['Junction'].map({True: 1, False: 0})
+df1['No_Exit'] = df['No_Exit'].map({True: 1, False: 0})
+df1['Railway'] = df['Railway'].map({True: 1, False: 0})
+df1['Roundabout'] = df['Roundabout'].map({True: 1, False: 0})
+df1['Station'] = df['Station'].map({True: 1, False: 0})
+df1['Stop'] = df['Stop'].map({True: 1, False: 0})
+df1['Traffic_Calming'] = df['Traffic_Calming'].map({True: 1, False: 0})
+df1['Turning_Loop'] = df['Turning_Loop'].map({True: 1, False: 0})
 
 #converter os dados de tempo para datetime
 df['Start_Time'] = pd.to_datetime(df['Start_Time'])
 df['End_Time'] = pd.to_datetime(df['End_Time'])
 #calcular a diferenca entre o tempo de inicio e fim do acidente
-df1['Acident_Duration'] = df['End_Time'] - df['Start_Time']
+df1['Acident_Duration'] = (df['End_Time'] - df['Start_Time']).dt.total_seconds().astype(int)
 
-print(df1)
-print(df1['Temperature(F)'])
-print(df1['Temperature(C)'])
+def describe():
+    # Ordena a severidade por quartis (25%, 50%(mediana), 75%)
+    describSever = df.describe()
+    print(describSever)
+
+    print(df1.describe())
+
+    print(df1.size)
+
+def filtros():
+    #Acidentes Por estado
+    countsAciSta = df1["State"].value_counts().head(10)
+    print(countsAciSta)
+
+    #Acidentes nas diferentes timezones
+    timezone_counts = df1['Timezone'].value_counts()
+    print(timezone_counts)
+
+    severity_humidity = df1.groupby('Severity')['Humidity(%)'].mean()
+    print(severity_humidity)
+
+    weather = df1['Weather_Condition'].value_counts().head(10)
+    print(weather)
+
+def grafico_acidentes_estado():
+    countsAciSta = df1["State"].value_counts()
+    countsAciSta.plot(kind="bar")
+    plt.title("Acidentes por estado")
+    plt.xlabel("State")
+    plt.ylabel("Count") 
+
+    plt.show()
+
+def correlacoes():  
+    num_df = df1.select_dtypes(include = 'number')
+    df2 = num_df.drop(columns=['Start_Lat', 'Start_Lng', 'End_Lat', 'End_Lng', 'Number'])
+    sns.heatmap(df2.corr(), annot=True, cmap='coolwarm', fmt='.2f')
+    plt.show()
+
+def dispersao_severidade_temperatura(): 
+    #cria grafico de dispersão entre a severidade e a temperatura em fahrenheit
+    sns.pairplot(df1[['Severity', 'Humidity(%)', 'Temperature(F)', 'Visibility(mi)']])
+    plt.show()
+
+
+def histograma_acidentes_por_mes():
+    #HISTOGRAMA E CONTAGEM DO NºACIDENTES POR MÊS
+    df1['Start_Time'] = pd.to_datetime(df['Start_Time']) # converte a coluna de data em um objeto de série de data
+    df1['Month_Year'] = df1['Start_Time'].dt.year.astype(str) + '/' + df1['Start_Time'].dt.month.astype(str)
+    x = df1['Month_Year'].value_counts() #Contagem do nºde acidentes por mês
+    x = x.sort_index()
+    x.plot(kind="bar")
+
+    #Adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição dos Acidentes de Carro ao Longo dos Meses')
+    plt.xlabel('Mês')
+    plt.ylabel('Número de Acidentes')
+
+    plt.show()
+
+def normalizacao():
+    plt.plot(df1['Severity'])
+    plt.xlim(0, 100)
+    plt.figure()
+    #fit scaler on data
+    normMinMax = MinMaxScaler()
+    # transform data
+    norm = normMinMax.fit_transform(df[['Severity']].values)
+    print(norm)
+    plt.plot(norm)
+    plt.xlim(0, 100)
+    plt.show()
+
+def boxPlots():
+    #SEVERIDADE-------------------------------------------------------
+    # cria um boxplot da coluna "Severity"
+    plt.boxplot(df1['Severity'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição da Severidade dos Acidentes')
+    plt.ylabel('Severidade')
+
+    # exibe o boxplot
+    plt.show()
+
+    #Wind_Chill-------------------------------------------------------
+    # cria um boxplot da coluna "Wind_Chill"
+    plt.boxplot(df1['Wind_Chill(F)'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição do wind chill')
+    plt.ylabel('Wind_Chill')
+
+    # exibe o boxplot
+    plt.show()
+
+    #Humidity-------------------------------------------------------
+    # cria um boxplot da coluna "humidity"
+    plt.boxplot(df1['Humidity(%)'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição da humidade')
+    plt.ylabel('Humidity')
+
+    # exibe o boxplot
+    plt.show()
+
+    #Pressão-------------------------------------------------------
+    # cria um boxplot da coluna "Pressao"
+    plt.boxplot(df1['Pressure(in)'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição da pressao atmosferica')
+    plt.ylabel('Pressão')
+
+    # exibe o boxplot
+    plt.show()
+
+    #Visibility-------------------------------------------------------
+    # cria um boxplot da coluna "Visibility"
+    plt.boxplot(df1['Visibility(mi)'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição da visibilidade')
+    plt.ylabel('Visibilidade')
+
+    # exibe o boxplot
+    plt.show()
+
+    #Wind_Speed-------------------------------------------------------
+    # cria um boxplot da coluna "Wind_Speed"
+    plt.boxplot(df1['Wind_Speed(mph)'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição da velocidade do vento')
+    plt.ylabel('wind_Speed')
+
+    # exibe o boxplot
+    plt.show()
+
+    #Precipitação-------------------------------------------------------
+    # cria um boxplot da coluna "Precipitação"
+    plt.boxplot(df1['Precipitation(in)'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição da precipitação')
+    plt.ylabel('Precipitação')
+
+    # exibe o boxplot
+    plt.show()
+
+    #Duração-----------------------------------------------------
+    # cria um boxplot da coluna "Duration"
+    plt.boxplot(df1['Acident_Duration'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição da Duração dos Acidentes')
+    plt.ylabel('Duração')
+
+    # exibe o boxplot
+    plt.show()
+
+    #Distancia-----------------------------------------------------
+    # cria um boxplot da coluna "Distancia"
+    plt.boxplot(df1['Distance(M)'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição da Distancia dos Acidentes')
+    plt.ylabel('Distancia')
+
+    # exibe o boxplot
+    plt.show()
+
+    #HUMIDADE-------------------------------------------------
+    # cria um boxplot da coluna "Humidity(%)"
+    plt.boxplot(df1['Humidity(%)'])
+
+    # adiciona títulos e rótulos aos eixos
+    plt.title('Distribuição da Severidade dos Acidentes')
+    plt.ylabel('Humidade(%)')
+
+    # exibe o boxplot
+    plt.show()
+
+def transformacao_linear():
+    plt.plot(df1['Distance(mi)'])
+    plt.title('Sem transformação linear')
+    plt.xlim(0, 100)
+    plt.figure()
+    plt.plot(df1['Distance(M)'])
+    plt.title('Com transformação linear')
+    plt.xlim(0, 100)
+    plt.show()
+
+#grafico_acidentes_estado()
+#histograma_acidentes_por_mes()
+#normalizacao()
+#transformacao_linear()
+#boxPlots()
+#describe()
+#filtros()
+#correlacoes()
+#dispersao_severidade_temperatura()
 
